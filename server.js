4161ae98c0aa0fcdb7bbc369e46e265b790c8f70
Kaan Berkay Bilgen <131668510+BerkayBilgenn@users.noreply.github.com>
2025-02-17 11:31:06 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const app = express();
+const path = require("path");
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Statik dosyaları sun
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 
-// Ana sayfa
+// Ana sayfa yönlendirmesi
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(path.join(__dirname, "Anasayfa", "main.html"));
 });
 
 // Sunucuyu başlat
